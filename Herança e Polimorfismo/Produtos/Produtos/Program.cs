66bbbc6a1607d6739300a0a97589af045c2fcbb0
Alexandre Maximo <alexandre.maximoms@gmail.com>
2022-01-24 00:38:41 -0300

    Adicionando .gitignore

---
+++ Herança e Polimorfismo/Produtos/Produtos/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Globalization;
+using System.Collections.Generic;
+using Produtos.Entities;
+
+List<Product> list = new List<Product>();
+
+Console.Write("Enter the number of products: ");
+int numProd = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= numProd; i++)
+{
+    Console.WriteLine($"Product #{i} data:");
+
+    Console.Write("Common, used or imported (c/u/i)? ");
+    char tpProduct = char.Parse(Console.ReadLine());
+
+    Console.Write("Name: ");
+    string name = Console.ReadLine();
+
+    Console.Write("Price: ");
+    double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+    if (tpProduct == 'i')
+    {
+        Console.Write("Customs fee: ");
+        double customsFee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        list.Add(new ImportedProduct(name, price, customsFee));
+    }
+    else if (tpProduct == 'u')
+    {
+        Console.Write("Manufacture date (DD/MM/YYYY): ");
+        DateOnly manDate = DateOnly.Parse(Console.ReadLine());
+        list.Add(new UsedProduct(name, price, manDate));
+    }
+    else
+    {
+        list.Add(new Product(name, price));
+    }
+
+}
+Console.WriteLine();
+Console.WriteLine("PRICE TAGS:");
+foreach (Product product in list)
+{
+    Console.WriteLine(product.priceTag());
+}
