b29dab0b37fb12462ea0941837f3cb61ae4064e7
Alexandre Maximo <alexandre.maximoms@gmail.com>
2022-01-24 00:28:54 -0300

    Exercício sobre Herança e Polimorfismo

---
+++ Herança e Polimorfismo/Produtos/Produtos/Entities/ImportedProduct.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Globalization;
+
+namespace Produtos.Entities
+{
+    internal class ImportedProduct : Product
+    {
+        public double CustomsFee { get; set; }
+
+        public ImportedProduct()
+        {
+        }
+
+        public ImportedProduct(string name, double price, double customsFee) : base(name, price)
+        {
+            CustomsFee = customsFee;
+        }
+        public override string priceTag()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(Name);
+            sb.Append(" $ " + totalPrice().ToString("F2", CultureInfo.InvariantCulture));
+            sb.Append(" (Customs fee: $ " + CustomsFee.ToString("F2",CultureInfo.InvariantCulture) + ")");
+
+            return sb.ToString();
+        }
+        public double totalPrice()
+        {
+            Price += CustomsFee;
+            return Price;
+        }
+    }
+}
---
+++ Herança e Polimorfismo/Produtos/Produtos/Entities/UsedProduct.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using System.Globalization;
+
+namespace Produtos.Entities
+{
+    internal class UsedProduct : Product
+    {
+        public DateOnly ManufactureDate { get; set; }
+
+        public UsedProduct()
+        {
+
+        }
+
+        public UsedProduct(string name, double price, DateOnly manufactureDate) : base(name, price)
+        {
+            ManufactureDate = manufactureDate;
+        }
+        public override string priceTag()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(Name + " (Used) ");
+            sb.Append(" $ " + Price.ToString("F2", CultureInfo.InvariantCulture));
+            sb.Append(" (Manufacture date: " + ManufactureDate.ToString("dd/MM/yyyy")+")");
+
+            return sb.ToString();
+        }
+    }
+}
