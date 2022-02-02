639a60353ed66fd5d3f5ece3d20028a08150d927
Alexandre Maximo <alexandre.maximoms@gmail.com>
2022-02-02 01:09:14 -0300

    Melhorando a implementação do método imprimirTabuleiro

--- Xadrez-Console/xadrez-console/xadrez-console/Program.cs
+++ Xadrez-Console/xadrez-console/xadrez-console/Program.cs
@@ -7,22 +7,22 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
-            Console.WriteLine(pos);
-            Console.WriteLine(pos.toPosicao());
+            //PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            //Console.WriteLine(pos);
+            //Console.WriteLine(pos.toPosicao());
 
-            //try
-            //{
-            //    Tabuleiro tab = new Tabuleiro(9, 9);
-            //    tab.colocarPecas(new Torre(Cor.Preta, tab), new Posicao(1, 1));
-            //    tab.colocarPecas(new Torre(Cor.Preta, tab), new Posicao(1, 3));
-            //    tab.colocarPecas(new Rei(Cor.Preta, tab), new Posicao(5, 8));
-            //    Tela.imprimirTabuleiro(tab);
-            //}
-            //catch (TabuleiroException e)
-            //{
-            //    Console.WriteLine(e.Message);
-            //}
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
+                tab.colocarPecas(new Torre(Cor.Preta, tab), new Posicao(1, 1));
+                tab.colocarPecas(new Torre(Cor.Preta, tab), new Posicao(1, 3));
+                tab.colocarPecas(new Rei(Cor.Branca, tab), new Posicao(5, 7));
+                Tela.imprimirTabuleiro(tab);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
         }
     }
--- Xadrez-Console/xadrez-console/xadrez-console/Tela.cs
+++ Xadrez-Console/xadrez-console/xadrez-console/Tela.cs
@@ -8,35 +8,38 @@ namespace xadrez_console
         {
             for (int l = 0; l < tab.linhas; l++)
             {
-                if (l == 0)
-                {
-                    Console.Write("x ");
-                }
-                else
-                {
-                    Console.Write(l.ToString() + " ");
-                }
+                Console.Write(8 - l + " ");
                 for (int c = 1; c < tab.colunas; c++)
                 {
-                    if (l == 0)
+
+                    if (tab.peca(l, c) == null)
                     {
-                        Console.Write(c.ToString() + " ");
+                        Console.Write("- ");
                     }
                     else
                     {
-                        if (tab.peca(l, c) == null)
-                        {
-                            Console.Write("- ");
-                        }
-                        else
-                        {
-                            Console.Write(tab.peca(l, c) + " ");
-                        }
+                        imprimirPeca(tab.peca(l, c));
+                        Console.Write(" ");
                     }
+
                 }
                 Console.WriteLine();
             }
-
+            Console.WriteLine(" a b c d e f g h");
+        }
+        public static void imprimirPeca(Peca peca)
+        {
+            if(peca.cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+            }
         }
     }
 }
