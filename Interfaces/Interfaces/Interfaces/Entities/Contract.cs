25b156e23afa9cdf79c0d9b14a05ed0b8cb03f0b
Alexandre Maximo <alexandre.maximoms@gmail.com>
2022-03-13 15:39:42 -0300

    Teste

---
+++ Interfaces/Interfaces/Interfaces/Entities/Contract.cs
@@ -0,0 +1,18 @@
+﻿namespace Interfaces
+{
+    internal class Contract
+    {
+        public int Number { get; set; } //Numero do contrato
+        public DateTime Date { get; set; } //Data de início do contrato
+        public double TotalValue { get; set; } // Valor total do contrato sem juros
+        public Installment Installment { get; set; }// Valor das prestações
+
+        public Contract(int number, DateTime date, double totalValue, Installment installment)
+        {
+            Number = number;
+            Date = date;
+            TotalValue = totalValue;
+            Installment = installment;
+        }
+    }
+}
